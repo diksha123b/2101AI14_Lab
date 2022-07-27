00b32e6b81f1f89eded6346986d46e0995ed4515
diksha123b <101545937+diksha123b@users.noreply.github.com>
2022-07-27 23:53:59 +0530

    Add files via upload

--- SORTING.c
+++ SORTING.c
@@ -51,6 +51,10 @@ int main(void)
         bubble_sort(arr,n);
         arr_print(arr,n);
         break;
+        case 4:
+        merge_sort(arr,0,n-1);
+        arr_print(arr,n);
+        break;
     
     default: printf("\nError");
     }
@@ -96,6 +100,45 @@ void bubble_sort(int arr[], int n)
                 arr[j+1]=temp;
             }
         }
+    }
+    return;
+}
+void merge(int arr[], int l, int m, int r)
+{
+    int i,j,k;
+    int n1=m-l+1;
+    int n2=r-m;
+    int arrL[n1], arrR[n2];
+
+    for (i=0; i<n1; i++){arrL[i]=arr[l+i];}
+    for (j=0; j<n2; j++){arrR[j]=arr[m+1+j];}
+
+    i=0;j=0;k=l;
+
+    while (i<n1 && j<n2){
+        if (arrL[i]<=arrR[j]){
+            arr[k]=arrL[i];
+            i++;
+        }
+        else{
+            arr[k]=arrR[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i<n1){arr[k]=arrL[i];i++;k++;}
+    while (j<n2){arr[k] = arrR[j];j++;k++;}
+    return;
+}
+ 
+void merge_sort(int arr[], int l, int r)
+{
+    if (l<r){
+        int m = l + (r - l) / 2;
+        merge_sort(arr, l, m);
+        merge_sort(arr, m + 1, r);
+        merge(arr, l, m, r);
     }
     return;
 }
